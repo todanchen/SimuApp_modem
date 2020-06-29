c95027172a93c768e0fc9e2301beab2ff724c552
todanchen <45915945+todanchen@users.noreply.github.com>
2020-06-29 15:08:08 +0800

    Update server629.py

--- server629.py
+++ server629.py
@@ -7,8 +7,8 @@ import sys
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('124.160.127.162', 8080))
-    # server.bind(('127.0.0.1', 8001))
+    server.bind(('124.160.127.162', 8000))
+    #server.bind(('127.0.0.1', 8001))
     server.listen(1)
 
     accept(server)
@@ -22,42 +22,43 @@ def accept(server):
     wlist = []
     xlist = []
     while True:
-        rs, ws, xs = select(rlist, wlist, xlist, 1)
-        for r in rs:
+        # rs, ws, xs = select(rlist, wlist, xlist, 1)
+        # for r in rs:
+        try:
+            flag = conn.recv(1024).decode()
+            date_rate = int(flag.split(',')[1])
+            Bj = int(flag.split(',')[2])
+        except:
+            pass
+        print(flag)
+        if not flag:
+            pass
+        elif 'initial' in flag:
+            v_data = bytes(date_rate*30)
+            s_data = v_data + 'start'.encode()
             try:
-                flag = conn.recv(1024).decode()
-                date_rate = int(flag.split(',')[1])
-                Bj = int(flag.split(',')[2])
+                conn.send(s_data)
+                Bi = 5
+                print('Initial the video, this time: ', time.time(), '------value_B: ', Bi)
             except:
                 pass
-            print(flag)
-            if not flag:
+        elif 'request' in flag:
+            if Bj >= 15:
+                sleeptime = 6*(Bi-15)
+                Bi = 15
+                print('need to sleep: ', sleeptime)
+                time.sleep(sleeptime)
+            v_data = bytes(date_rate * 6)
+            s_data = v_data + 'end'.encode()
+            try:
+                conn.send(s_data)
+                Bi += 1
+                print('Send the next video clice, this time: ', time.time(), '------value_B: ', Bi)
+            except:
                 pass
-            elif 'initial' in flag:
-                v_data = bytes(date_rate*30)
-                s_data = v_data + 'start'.encode()
-                try:
-                    conn.send(s_data)
-                    Bi = 5
-                    print('Initial the video, this time: ', time.time(), '------value_B: ', Bi)
-                except:
-                    pass
-            elif 'request' in flag:
-                if Bj >= 15:
-                    sleeptime = 6*(Bi-15)
-                    Bi = 15
-                    time.sleep(sleeptime)
-                v_data = bytes(date_rate * 6)
-                s_data = v_data + 'end'.encode()
-                try:
-                    conn.send(s_data)
-                    Bi += 1
-                    print('Send the next video clice, this time: ', time.time(), '------value_B: ', B)
-                except:
-                    pass
-            elif 'Exit' in flag:
-                conn.close()
-                break
+        elif 'Exit' in flag:
+            conn.close()
+            break
 
 
 
